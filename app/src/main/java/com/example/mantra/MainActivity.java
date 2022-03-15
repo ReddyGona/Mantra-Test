b9fa5ae379dc72cd9addf306e8d7cdf99b8bd698
ReddyGona <gonajanardhan@karunya.edu.in>
2022-03-15 19:02:02 +0530

    finger print stored and retrieved from data base and matched successfully

--- app/src/main/java/com/example/mantra/MainActivity.java
+++ app/src/main/java/com/example/mantra/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.mantra;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.Nullable;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
@@ -10,20 +11,41 @@ import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.mantra.mfs100.FingerData;
 import com.mantra.mfs100.MFS100;
 import com.mantra.mfs100.MFS100Event;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends Activity implements MFS100Event {
 
-    Button cap, stop_cap, match;
-    TextView l_message, textView2;
+    Button cap, stop_cap, match, save;
+    TextView l_message, textView1, textView2;
     ImageView ImageFinger;
+    EditText Name;
 
+
+    String api_savefinger="https://www.schedular.in/MBS/USER/mfs_demo.php";
+    String api_match="https://www.schedular.in/MBS/USER/mfs_match.php";
+    String finger1="";
 
 
     private enum ScannerAction {
@@ -54,19 +76,22 @@ public class MainActivity extends Activity implements MFS100Event {
 
         cap = findViewById(R.id.button);
         stop_cap = findViewById(R.id.button2);
-        l_message = findViewById(R.id.textView);
+        l_message = findViewById(R.id.textView2);
         ImageFinger = findViewById(R.id.imgFinger);
         match = findViewById(R.id.match);
-        textView2 = findViewById(R.id.textView2);
+        textView2 = findViewById(R.id.textView1);
+        save=findViewById(R.id.save);
+        Name=findViewById(R.id.name);
+        textView1=findViewById(R.id.textView1);
 
         match.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                ImageFinger.setImageResource(R.drawable.ic_baseline_fingerprint_24);
                 new Thread(new Runnable() {
-
                     @Override
                     public void run() {
-                        SetTextOnUIThread("");
+                        SetTextOnUIThread("started.. matching.......");
                         isCaptureRunning = true;
                         try {
                             FingerData fingerData = new FingerData();
@@ -86,8 +111,6 @@ public class MainActivity extends Activity implements MFS100Event {
                                         match_finger();
                                     }
                                 });
-
-
                             }
                         } catch (Exception ex) {
                             SetTextOnUIThread("Error");
@@ -106,7 +129,7 @@ public class MainActivity extends Activity implements MFS100Event {
 
                     @Override
                     public void run() {
-                        SetTextOnUIThread("");
+                        SetTextOnUIThread("capturing.....");
                         isCaptureRunning = true;
                         try {
                             FingerData fingerData = new FingerData();
@@ -122,12 +145,12 @@ public class MainActivity extends Activity implements MFS100Event {
                                     @Override
                                     public void run() {
                                         ImageFinger.setImageBitmap(bitmap);
+                                        SetTextOnUIThread("capture..success....");
                                     }
                                 });
 
-                                Enroll_Template = fingerData.ISOTemplate();
-                                textView2.setText(Enroll_Template.toString());
-
+                                 finger1 = Base64.getEncoder().encodeToString(fingerData.ISOTemplate());
+                              //  Enroll_Template= Base64.getDecoder().decode(finger1);
                             }
                         } catch (Exception ex) {
                             SetTextOnUIThread("Error");
@@ -146,21 +169,126 @@ public class MainActivity extends Activity implements MFS100Event {
             }
         });
 
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ProgressDialog progressDialog = new ProgressDialog(MainActivity.this);
+                progressDialog.setMessage("Saving details");
+                progressDialog.show();
+
+                StringRequest save_data =new StringRequest(Request.Method.POST, api_savefinger, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            String status = jsonObject.getString("status");
+                            JSONArray jsonArray=jsonObject.getJSONArray("details");
+                            if (status.equals("1")){
+                                progressDialog.dismiss();
+                                Enroll_Template=null;
+                                Toast.makeText(MainActivity.this, "success: ", Toast.LENGTH_SHORT).show();
+                            }else {
+                                progressDialog.dismiss();
+                                Toast.makeText(MainActivity.this, "not success : "+status, Toast.LENGTH_SHORT).show();
+                            }
+
+                        } catch (JSONException e) {
+                            Toast.makeText(MainActivity.this, "catch : "+e.getMessage(), Toast.LENGTH_SHORT).show();
+                            progressDialog.dismiss();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        progressDialog.dismiss();
+                        Toast.makeText(MainActivity.this, "network error", Toast.LENGTH_SHORT).show();
+                    }
+                }){
+                    @Nullable
+                    @Override
+                    protected Map<String, String> getParams() throws AuthFailureError {
+                        Map<String, String> data = new HashMap<>();
+                        data.put("name", Name.getText().toString());
+                        data.put("finger1", finger1);
+                        return data;
+                    }
+                };
+
+                RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
+                requestQueue.add(save_data);
+            }
+        });
+
+
     }
 
     private void match_finger() {
-        Toast.makeText(this, "into match", Toast.LENGTH_SHORT).show();
-        int ret = mfs100.MatchISO(Enroll_Template, Verify_Template);
-        if (ret < 0) {
-            SetTextOnUIThread("Error: " + ret + "(" + mfs100.GetErrorMsg(ret) + ")");
-        } else {
-            if (ret >= 1400) {
-                SetTextOnUIThread("Finger matched with score: " + ret);
-                return;
-            } else {
-                SetTextOnUIThread("Finger not matched, score: " + ret);
+
+        StringRequest match = new StringRequest(Request.Method.POST, api_match, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                String finger="";
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    String status = jsonObject.getString("status");
+                    JSONArray jsonArray=jsonObject.getJSONArray("details");
+                    Toast.makeText(MainActivity.this, "status : "+status, Toast.LENGTH_SHORT).show();
+                    if (status.equals("1")){
+                        for (int i=0; i<jsonArray.length(); i++){
+                            JSONObject object=jsonArray.getJSONObject(i);
+                             finger = object.getString("finger");
+                        }
+
+                        Enroll_Template=Base64.getDecoder().decode(finger);
+                        Toast.makeText(MainActivity.this, "matching", Toast.LENGTH_SHORT).show();
+                        int ret = mfs100.MatchISO(Enroll_Template, Verify_Template);
+                        if (ret < 0) {
+                            SetTextOnUIThread("Error: " + ret + "(" + mfs100.GetErrorMsg(ret) + ")");
+                        } else {
+                            if (ret >= 1400) {
+                                SetTextOnUIThread("Finger matched with score: " + ret);
+                                return;
+                            } else {
+                                SetTextOnUIThread("Finger not matched, score: " + ret);
+                            }
+                        }
+
+                    }
+                } catch (JSONException e) {
+                    Toast.makeText(MainActivity.this, "catch : "+e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+
             }
-        }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this, "network error", Toast.LENGTH_SHORT).show();
+            }
+        }){
+            @Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> data = new HashMap<>();
+                data.put("name", Name.getText().toString());
+                return data;
+            }
+        };
+        RequestQueue requestQueue=Volley.newRequestQueue(MainActivity.this);
+        requestQueue.add(match);
+
+
+
+
+    }
+
+    private void SetTextOnUIThread(final String str) {
+
+        l_message.post(new Runnable() {
+            public void run() {
+                l_message.setText(str);
+            }
+        });
     }
 
     @Override
@@ -268,13 +396,6 @@ public class MainActivity extends Activity implements MFS100Event {
         }
     }
 
-    private void SetTextOnUIThread(final String str) {
 
-        l_message.post(new Runnable() {
-            public void run() {
-                l_message.setText(str);
-            }
-        });
-    }
 
 }
